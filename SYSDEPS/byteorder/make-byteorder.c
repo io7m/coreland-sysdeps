5ff81bad9a4eb4e1c4d40a78438f0882a1157b1d
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2006-10-13 09:28:16 +0000

    endian -> byteorder
    git-svn-id: file:///source/coreland/sysdeps/trunk@1120 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- SYSDEPS/byteorder/make-byteorder.c
+++ SYSDEPS/byteorder/make-byteorder.c
@@ -8,19 +8,19 @@ int main()
   i = 127; 
   b = *(char*)&i;
   
-  printf("#ifndef _ENDIANNESS_H\n");
-  printf("#define _ENDIANNESS_H\n");
+  printf("#ifndef _BYTEORDER_H\n");
+  printf("#define _BYTEORDER_H\n");
   printf("\n");
 
   printf("#define E_LITTLE_ENDIAN 1\n");
   printf("#define E_BIG_ENDIAN 0\n");
 
   if(b == i) {
-    printf("#define ENDIANNESS E_LITTLE_ENDIAN\n");
-    fprintf(stderr,"endian:little\n");
+    printf("#define BYTEORDER E_LITTLE_ENDIAN\n");
+    fprintf(stderr,"byteorder:little\n");
   } else {
-    printf("#define ENDIANNESS E_BIG_ENDIAN\n");
-    fprintf(stderr,"endian:big\n");
+    printf("#define BYTEORDER E_BIG_ENDIAN\n");
+    fprintf(stderr,"byteorder:big\n");
   }
 
   printf("\n");
