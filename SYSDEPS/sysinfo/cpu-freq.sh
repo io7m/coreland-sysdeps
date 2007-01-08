ea61af723532aa89d7632499b9bf2d0adfd05ba7
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-01-08 14:30:23 +0000

    freq
    git-svn-id: file:///source/coreland/sysdeps/trunk@1410 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

---
+++ SYSDEPS/sysinfo/cpu-freq.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+. ../sysdep-subs
+if [ $? -ne 0 ]
+then
+  exit 111
+fi
+
+if [ x"$1" = x ]
+then
+  exit 1
+fi
+
+exec 2>/dev/null
+sys="$1"
+freq=0
+
+case "$sys" in
+  SYSINFO_OS_DARWIN)
+    freq="`sysctl hw.cpufrequency | awk '{print $2}'`"
+    ;; 
+esac
+
+echo "$freq"
