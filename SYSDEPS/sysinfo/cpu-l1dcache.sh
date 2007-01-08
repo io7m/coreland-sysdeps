ac74c02683a56aa492c31cbec8fd8a8e3d65352c
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-01-08 14:21:32 +0000

    split caches
    git-svn-id: file:///source/coreland/sysdeps/trunk@1409 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

---
+++ SYSDEPS/sysinfo/cpu-l1dcache.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+. ../sysdep-subs
+if [ $? -ne 0 ]
+then
+  exit 111
+fi
+
+if [ x"$1" = x ]
+then
+  exit 1
+fi
+
+exec 2>/dev/null
+sys="$1"
+cache=0
+
+case "$sys" in
+  SYSINFO_OS_DARWIN)
+    cache="`sysctl hw.l1dcachesize | awk '{print $2}'`"
+    ;; 
+esac
+
+echo "$cache"
