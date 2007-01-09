e2b66a550d33f0a581e8dd407bc8b4064abc330d
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-01-09 04:03:44 +0000

    have
    git-svn-id: file:///source/coreland/sysdeps/trunk@1427 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- SYSDEPS/sysinfo/cpu-feat.sh
+++ SYSDEPS/sysinfo/cpu-feat.sh
@@ -20,33 +20,31 @@ case "$sys" in
     alti="`sysctl hw.optional.altivec | awk '{print $2}'`"
     if [ "${alti}" = "1" ]
     then
-      feat="${feat} | SYSINFO_CPU_PPC_EXT_ALTIVEC";
+      feat="${feat} | SYSINFO_CPU_EXT_ALTIVEC";
     fi
-    ;; 
-  *)
-    # assume x86
-    ${SYSDEP_CC} -o cpuid_x86 cpuid_x86.c
-    if [ $? -eq 0 ]
-    then
-      f=`./cpuid_x86 mmx`
-      if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_X86_EXT_MMX"; fi
-      f=`./cpuid_x86 mmx2`
-      if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_X86_EXT_MMX2"; fi
-      f=`./cpuid_x86 3dnow`
-      if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_X86_EXT_3DNOW"; fi
-      f=`./cpuid_x86 3dnowext`
-      if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_X86_EXT_3DNOWEXT"; fi
-      f=`./cpuid_x86 sse`
-      if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_X86_EXT_SSE"; fi
-      f=`./cpuid_x86 sse2`
-      if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_X86_EXT_SSE2"; fi
-      f=`./cpuid_x86 sse3`
-      if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_X86_EXT_SSE3"; fi
-      f=`./cpuid_x86 sse4`
-      if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_X86_EXT_SSE4"; fi
-      rm -f cpuid_x86
-    fi
-    ;;
+  ;;
 esac
 
+${SYSDEP_CC} -o cpuid_x86 cpuid_x86.c
+if [ $? -eq 0 ]
+then
+  f=`./cpuid_x86 mmx`
+  if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_EXT_MMX"; fi
+  f=`./cpuid_x86 mmx2`
+  if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_EXT_MMX2"; fi
+  f=`./cpuid_x86 3dnow`
+  if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_EXT_3DNOW"; fi
+  f=`./cpuid_x86 3dnowext`
+  if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_EXT_3DNOWEXT"; fi
+  f=`./cpuid_x86 sse`
+  if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_EXT_SSE"; fi
+  f=`./cpuid_x86 sse2`
+  if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_EXT_SSE2"; fi
+  f=`./cpuid_x86 sse3`
+  if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_EXT_SSE3"; fi
+  f=`./cpuid_x86 sse4`
+  if [ $f -eq 1 ]; then feat="${feat} | SYSINFO_CPU_EXT_SSE4"; fi
+  rm -f cpuid_x86
+fi
+ 
 echo "${feat})"
