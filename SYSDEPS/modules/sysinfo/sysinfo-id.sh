2a018916a272970a8e49cc503ad41a7409ba9c86
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-01-13 13:20:07 +0000

    sysinfo sysdep line
    git-svn-id: file:///source/coreland/sysdeps/trunk@1533 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

---
+++ SYSDEPS/modules/sysinfo/sysinfo-id.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ ! -f "_sysinfo.h" ]
+then
+  echo "fatal: could not open _sysinfo.h" 1>&2
+  exit 112
+fi
+
+os=`grep 'SYS_OS ' _sysinfo.h \
+ | sed -e 's/SYS_OS_//g' | awk '{print $NF}'`
+ver=`grep 'SYS_OSVERSION ' _sysinfo.h \
+ | sed -e 's/SYS_OSVERSION_//g' | awk '{print $NF}'`
+cc=`grep 'SYS_CCTYPE ' _sysinfo.h \
+ | sed -e 's/SYS_CCTYPE_//g' | awk '{print $NF}'`
+arch=`grep 'SYS_ARCH ' _sysinfo.h \
+ | sed -e 's/SYS_ARCH_//g' | awk '{print $NF}'`
+
+printf "%s %s %s %s\n" "${os}" "${ver}" "${cc}" "${arch}" \
+  | tr [A-Z] [a-z] | sed -e 's/"//g'
