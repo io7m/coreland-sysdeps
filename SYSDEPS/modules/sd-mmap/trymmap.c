8f6e5362f15fcade71cd0f64aca0144b4cc6d61d
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-02-24 12:52:03 +0000

    mmap fixes
    git-svn-id: file:///source/coreland/sysdeps/trunk@1781 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- SYSDEPS/modules/sd-mmap/trymmap.c
+++ SYSDEPS/modules/sd-mmap/trymmap.c
@@ -1,10 +1,25 @@
 #include <sys/types.h>
 #include <sys/mman.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/mman.h>
 
 int main()
 {
   void *map;
-  map = mmap(0, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+  int fd;
 
-  return (map == MAP_FAILED);
+  fd = open("/dev/zero", O_RDONLY);
+  if (fd == -1) {
+    perror("open");
+    return 1;
+  }
+   
+  map = mmap(0, 4096, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (map == MAP_FAILED) {
+    perror("mmap");
+    return 1;
+  } else return 0;
 }
