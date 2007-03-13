185260e947a887740f66790633fd3ff3c702fc94
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-03-13 13:18:15 +0000

    test for posix realtime threads
    git-svn-id: file:///source/coreland/sysdeps/trunk@1893 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- SYSDEPS/modules/sd-pthreads/tryptrealtime.c
+++ SYSDEPS/modules/sd-pthreads/tryptrealtime.c
@@ -1,8 +1,10 @@
 #include <pthread.h>
+#include <errno.h>
 #include <stdio.h>
 
 #define NUM_THREADS 5
 
+pthread_attr_t attr;
 pthread_t threads[NUM_THREADS];
 unsigned int ids[NUM_THREADS];
  
@@ -16,8 +18,9 @@ int main(void)
 {
   unsigned int ind;
   int rc;
+  int policy;
 
-  for(ind = 0; ind < NUM_THREADS; ind++){
+  for (ind = 0; ind < NUM_THREADS; ind++) {
     printf("[%u] thread create\n", ind);
     ids[ind] = ind;
     rc = pthread_create(&threads[ind], 0, hello, &ids[ind]);
@@ -26,6 +29,19 @@ int main(void)
       return 1;
     }
   }
+
+  rc = pthread_attr_init(&attr);
+  if (rc != 0) {
+    printf("error: attr_init: %s\n", strerror(rc));
+    return 1;
+  }
+
+  rc = pthread_attr_getschedpolicy(&attr, &policy);
+  if (rc != 0) {
+    printf("error: getschedpolicy: %s\n", strerror(rc));
+    return 1;
+  }
+
   pthread_exit(0);
   return 0;
 }
