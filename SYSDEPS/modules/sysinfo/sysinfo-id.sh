ecf193cfab1a2c6bd2caf3c63f8937ecbff6c6ec
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-03-28 21:15:29 +0000

    added sd-ccversion
    git-svn-id: file:///source/coreland/sysdeps/trunk@2070 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- SYSDEPS/modules/sysinfo/sysinfo-id.sh
+++ SYSDEPS/modules/sysinfo/sysinfo-id.sh
@@ -12,8 +12,10 @@ ver=`grep 'SYS_OSVERSION ' _sysinfo.h \
  | sed -e 's/SYS_OSVERSION_//g' | awk '{print $NF}'`
 cc=`grep 'SYS_CCTYPE ' _sysinfo.h \
  | sed -e 's/SYS_CCTYPE_//g' | awk '{print $NF}'`
+ccver=`grep 'SYS_CCVERSION ' _sysinfo.h \
+ | sed -e 's/SYS_CCVERSION//g' | awk '{print $NF}'`
 arch=`grep 'SYS_ARCH ' _sysinfo.h \
  | sed -e 's/SYS_ARCH_//g' | awk '{print $NF}'`
 
-printf "%s %s %s %s\n" "${os}" "${ver}" "${cc}" "${arch}" \
+printf "%s %s %s %s %s\n" "${os}" "${ver}" "${cc}" "${ccver}" "${arch}" \
   | tr [A-Z] [a-z] | sed -e 's/"//g'
