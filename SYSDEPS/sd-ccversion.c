f8b7d688b8a987d4660260d4b8ade818d47c9e08
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-04-28 09:12:21 +0000

    compaq C missing semicolons
    git-svn-id: file:///source/coreland/sysdeps/trunk@2201 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- SYSDEPS/sd-ccversion.c
+++ SYSDEPS/sd-ccversion.c
@@ -37,23 +37,23 @@ int main(void)
   /* Compaq C */
 #if defined(__DECC_VER)
   ver = __DECC_VER;
-  ver_major = ver / 10000000
-  ver -= ver_major * 10000000
-  ver_minor = ver / 100000
-  ver -= ver_minor * 100000
-  ver -= (ver / 10000) * 10000
-  ver_patch = ver
+  ver_major = ver / 10000000;
+  ver -= ver_major * 10000000;
+  ver_minor = ver / 100000;
+  ver -= ver_minor * 100000;
+  ver -= (ver / 10000) * 10000;
+  ver_patch = ver;
 #endif
 
   /* Compaq C++ */
 #if defined(__DECCXX_VER)
   ver = __DECCXX_VER;
-  ver_major = ver / 10000000
-  ver -= ver_major * 10000000
-  ver_minor = ver / 100000
-  ver -= ver_minor * 100000
-  ver -= (ver / 10000) * 10000
-  ver_patch = ver
+  ver_major = ver / 10000000;
+  ver -= ver_major * 10000000;
+  ver_minor = ver / 100000;
+  ver -= ver_minor * 100000;
+  ver -= (ver / 10000) * 10000;
+  ver_patch = ver;
 #endif
 
   /* Cray C */
