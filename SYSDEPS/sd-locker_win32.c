fbc0c481532f5aed4667144792541978f6b5baea
m0 <m0@cobra_winxp.internal.network>
2009-09-14 17:35:55 +0100

    Fix Win32 implementation of sd-locker.

--- SYSDEPS/sd-locker_win32.c
+++ SYSDEPS/sd-locker_win32.c
@@ -37,21 +37,23 @@ usage (void)
 static const char *lock_file;
 static HANDLE      lock_handle;
 
-static char *
+static const char *
 error_message (void)
 {
-  char *error_buffer = NULL;
+  char *buffer = NULL;
+  DWORD error_code = GetLastError ();
+
   FormatMessage
-   (FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+   (FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
     NULL,
-    GetLastError (),
+    error_code,
     0,
-    (LPTSTR) &error_buffer,
+    (LPTSTR) &buffer,
     0,
     0);
 
-  assert (error_buffer != NULL);
-  return error_buffer;
+  assert (buffer != NULL);
+  return buffer;
 }
 
 static void
@@ -69,80 +71,93 @@ die_unlock (int code, const char *message)
   exit (code);
 }
 
+#define POSIX_SHELL_PREFIX "sh -c "
+
 static char *
-flatten_command (int argc, char *argv[])
+convert_command (int argc, char *argv[])
 {
   size_t length_total;
   size_t length_param;
+  size_t length_used;
   char *buffer;
   char *ptr;
   int index;
 
-  /* Calculate total length of string array when flattened, plus NULL,
-   * plus a space between each argument.
-   */
-
-  length_total = 1;
+  /* Calculate required command line length. */
+  length_used  = 0;
+  length_total = sizeof (POSIX_SHELL_PREFIX);
   for (index = 0; index < argc; ++index)
     length_total += strlen (argv [index]) + 1;
 
-  assert (length_total > 1);
-
-  /* Allocate buffer. */
+  /* Allocate space for command line. */
+  assert (length_total > sizeof (POSIX_SHELL_PREFIX));
   buffer = malloc (length_total);
   if (buffer == NULL) return NULL;
 
-  /* Flatten array into buffer. */
-  ptr = buffer;
+  /* Copy POSIX shell prefix to command line. */
+  memcpy (buffer, POSIX_SHELL_PREFIX, sizeof (POSIX_SHELL_PREFIX) - 1);
+  ptr         = buffer + sizeof (POSIX_SHELL_PREFIX) - 1;
+  length_used = sizeof (POSIX_SHELL_PREFIX) - 1;
+
+  /* Copy each string into buffer from argument vector. */
   for (index = 0; index < argc; ++index) {
     length_param = strlen (argv [index]);
     memcpy (ptr, argv [index], length_param);
-    ptr += length_param;
-    *ptr = ' ';
-    ptr += 1;
+    ptr         += length_param;
+    length_used += length_param;
+    *ptr         = ' ';
+    ptr         += 1;
+    length_used += 1;
   }
 
-  assert (ptr == buffer + length_total - 1);
-
   /* Null terminate. */
-  *ptr = 0;
+  *ptr         = 0;
+  length_used += 1;
+
+  /* Various buffer assertions. */
+  assert (buffer [length_used - 1] == 0);
+  assert (length_used == length_total);
+  assert (ptr - buffer + 1 == (ptrdiff_t) length_total);
+
   return buffer;
 }
+
+static DWORD exit_code = EXIT_SUCCESS;
 
 static void
 execute (int argc, char *argv[])
 {
-  STARTUPINFO         startup_info;
-  PROCESS_INFORMATION process_info;
-  char                *flattened_line = NULL;
-  int                 index;
+  STARTUPINFO         info_startup;
+  PROCESS_INFORMATION info_process;
+  char *command;
+  BOOL exec_result;
+  DWORD wait_result;
 
-  flattened_line = flatten_command (argc, argv);
-  if (flattened_line == NULL) die_unlock (EXIT_FAILURE, "malloc");
+  ZeroMemory (&info_startup, sizeof (info_startup));
+  ZeroMemory (&info_process, sizeof (info_process));
+  info_startup.cb = sizeof (info_startup);
 
-  ZeroMemory (&startup_info, sizeof (startup_info));
-  ZeroMemory (&process_info, sizeof (process_info));
+  command = convert_command (argc, argv);
 
-  startup_info.cb = sizeof (startup_info);
+  exec_result = CreateProcess
+    (NULL,                  /* Command name. NULL == Use command line. */
+     command,               /* Command line. */
+     NULL,                  /* Process security attributes. */
+     NULL,                  /* Thread security attributes. */
+     TRUE,                  /* Inherit file handles. */
+     0,                     /* Creation flags. */
+     NULL,                  /* Environment. */
+     NULL,                  /* Current directory. */
+     &info_startup,
+     &info_process);
+  if (exec_result == FALSE) die_unlock (EXIT_FAILURE, "create_process");
 
-  if (FALSE == CreateProcess
-   (flattened_line,        /* Command name. */
-    NULL,                  /* Command line (points to flattened_line if NULL). */
-    NULL,                  /* Process security attributes. */
-    NULL,                  /* Thread security attributes. */
-    TRUE,                  /* Inherit file handles. */
-    NORMAL_PRIORITY_CLASS, /* Process priority. */
-    NULL,                  /* Environment. */
-    NULL,                  /* Current directory. */
-    &startup_info,         /* Process startup info. */
-    &process_info))        /* Process information. */
-  {
-    die_unlock (EXIT_FAILURE, "execute");
-  }
+  wait_result = WaitForSingleObject (info_process.hProcess, INFINITE);
+  /* XXX: Do something with wait_result here? */
 
-  if (WaitForSingleObject (process_info.hProcess, INFINITE) == WAIT_FAILED) die_unlock (EXIT_FAILURE, "wait");
-  if (CloseHandle (process_info.hProcess) == FALSE) die_unlock (EXIT_FAILURE, "close_handle");
-  if (CloseHandle (process_info.hThread) == FALSE)  die_unlock (EXIT_FAILURE, "close_handle");
+  if (GetExitCodeProcess (info_process.hProcess, &exit_code) == FALSE) die_unlock (EXIT_FAILURE, "exit_code");
+  if (CloseHandle (info_process.hProcess) == FALSE) die_unlock (EXIT_FAILURE, "close_handle");
+  if (CloseHandle (info_process.hThread) == FALSE) die_unlock (EXIT_FAILURE, "close_handle");
 }
 
 int
@@ -162,5 +177,6 @@ main (int argc, char *argv[])
 
   if (fd_unlock_w (lock_handle) == FALSE) die_unlock (EXIT_FAILURE, "unlock");
   if (CloseHandle (lock_handle) == FALSE) die_unlock (EXIT_FAILURE, "close");
-  return EXIT_SUCCESS;
+
+  return exit_code;
 }
